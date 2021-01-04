20198348e6d3cf26f7879411fdb25b80c97c2278
Alexandra Peccia <ampeccia@gmail.com>
2021-01-04 05:35:34 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -14,11 +14,7 @@ def fourth_challenge
           :name => "Mercutio",
           :age => "18",
           :attitude => "hot-headed"
-      
-      
-        
-        
-        
+        }
       ]
    }, 
    :capulet => {
